b7a3d3525ce71fdec88c98318e73c97e8138206f
Toua Thao <touathao09@gmail.com>
2019-05-23 12:56:04 -0500

    middleware is finish

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express')
+
+const app = express();
+
+const morgan = require('morgan')
+
+//bring in route
+const {getPosts} = require('./routes/post')
+
+//middleware variable
+const myMiddleWare = (req, res, next)=>{
+    console.log('middleWare apply')
+    next()
+}
+
+//middleware 
+/*
+what is a middleware? it's something between start and finish
+we could use it to check or do in the middle. like loging message ,etc
+*/
+app.use(morgan('dev'));
+app.use(myMiddleWare)
+
+app.get('',getPosts)
+
+const port= 8080
+app.listen(port, () =>{
+    console.log(`app listen is here from port:${port}`)
+}) 
